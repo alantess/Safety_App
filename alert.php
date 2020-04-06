7486fcbd29df8b2492c6ac3409d3dfd7acc0d493
Daniel Leach <dleach2018@fau.edu>
2020-04-06 18:50:23 -0400

    updating alert fetching

--- alert.php
+++ alert.php
@@ -2,22 +2,17 @@
 require_once('db_connect.php');
 session_start();
 
-
-
-
-
 if(isset($_POST('alert'))){
     $alert = 1;
-    $query = "INSERT INTO ALERT_LOG() 
-               VALUES(:alert)";
+    $t_start = date(DATE_RFC822);
+    $query = "INSERT INTO ALERT_LOG(CAT,T_START) 
+               VALUES(:cat,:t_start)";
     $stid = oci_parse($conn, $query);
-    oci_bind_by_name($stid, ":alert", $alert);
-    
+    oci_bind_by_name($stid, ":cat", $alert);
+    oci_bind_by_name($stid,':t_start',$t_start);
     
     oci_execute($stid);
     oci_free_statement($stid);
     oci_close($conn);
 }
-
-
 ?>
--- fetchAlert.php
+++ fetchAlert.php
@@ -1,15 +1,45 @@
 <?php
 include('db_connect.php');
 
-
-
 if(isset($_POST['alert'])){
-    if ($_POST('alert' != '')) {
+    $query = "SELECT * FROM ALERT_LOG WHERE T_END = NULL";
+    $stid = oci_parse($conn,$query);
+    oci_execute($stid);
+    $row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS);
+    $output = '';
 
-        $update_query = "UPDATE ALERT_LOG SET CAT =  WHERE comment_status=0";
-        mysqli_query($con, $update_query);
+    if($result > 0){
+        if($result['CAT'] == 0){
+            echo "Shooter on campus";
+            $output ='Shooter on campus';
+
+        }
+        if($result['CAt'] == 1){
+            echo "fire on campus";
+            
+        }
+        if ($result['CAt'] == 2) {
+            echo "natural on campus";
+            
+        }
+        if ($result['CAt'] == 3) {
+            echo "Medical Alert";
+            
+        }
+        if ($result['CAt'] == 4) {
+            echo "Fight Alert";
+            
+        }
+
+        $data = array(
+            'alert'=> $output
+        );
+        echo json_encode($data);
 
     }
+
+
+    
 }
 
 
