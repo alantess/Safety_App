dc6562e31e56cf80a53b1df76697a2cf46ed05d3
Daniel Leach <dleach2018@fau.edu>
2020-04-09 17:08:08 -0400

    working through problems

--- alert.php
+++ alert.php
@@ -21,6 +21,7 @@ if(isset($_POST['view'])){
         
     }else if($_POST['view']== 'NO'){
 
+
         if ($_SESSION['userlevel'] == 1) {
             $t_end = date(DATE_RFC822);
             $_seen = 'NO';
@@ -35,26 +36,30 @@ if(isset($_POST['view'])){
             $stid = oci_parse($conn,$query);
             oci_execute($stid);
             $result = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS);
-            // $isact = $result['IS_ACT'];
-
-            $isact = 'N';
-
-            $data = array(
-                'act' => $isact
-            );
+            $isact = $result['IS_ACT'];
 
 
-            echo json_encode($data);
+            // $isact = 'N';
+
+            // $data = array(
+            //     'act' => $isact
+            // );
+
+
+            // echo json_encode($data);
             oci_free_statement($stid);
 
 
+        }else{
+
         }
-        // header('location: home.php');
+       
     }else{
 
         // $query = "SELECT * FROM ALERT_LOG WHERE T_END IS NULL";
+        
 
-        $query = "SELECT * FROM ALERT_LOG WHERE T_START = (SELECT MAX(T_START) FROM ALERT_LOG)and IS_ACT ='Y'";
+        $query = "SELECT * FROM ALERT_LOG WHERE T_START = (SELECT MAX(T_START) FROM ALERT_LOG)";
 
         $stid = oci_parse($conn, $query);
         oci_execute($stid);
@@ -63,67 +68,70 @@ if(isset($_POST['view'])){
         $isact = $result['IS_ACT'];
         
 
-        if ($result > 0){
-        switch($result['CAT']){
-            case 0 :
-                $output .= '
-        <div class="container">
-            <div class="row">
-            <div class="alert alert-danger alert-dismissible" role="alert">
-                <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode);" class="close" data-dismiss="alert">
-                <span aria-hidden="true">×</span>
-                <span class="sr-only">Close</span>
-                </button>
-                <strong><i class="fa fa-warning"></i> Danger!</strong> <marquee><p style="font-family: Impact; font-size: 18pt">Active Shooter on Campus!</p></marquee>
-                </div>
-            </div>
-        </div>';
-            break;
+            if ($result > 0){
+                switch($result['CAT']){
+                    case 0 :
+                        $output .= '
+                <div class="container">
+                    <div class="row">
+                    <div class="alert alert-danger alert-dismissible" role="alert">
+                        <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode);" class="close" data-dismiss="alert">
+                        <span aria-hidden="true">×</span>
+                        <span class="sr-only">Close</span>
+                        </button>
+                        <strong><i class="fa fa-warning"></i> Danger!</strong> <marquee><p style="font-family: Impact; font-size: 18pt">Active Shooter on Campus!</p></marquee>
+                        </div>
+                    </div>
+                </div>';
+                    break;
 
-            case 1:
-                $output .= '
-        <div class="container">
-            <div class="row">
-            <div class="alert alert-danger alert-dismissible" role="alert">
-                <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode);" class="close" data-dismiss="alert">
-                <span aria-hidden="true">×</span>
-                <span class="sr-only">Close</span>
-                </button>
-                <strong><i class="fa fa-warning"></i> Danger!</strong> <marquee><p style="font-family: Impact; font-size: 18pt">Fire on Campus! Evacuate Immediately</p></marquee>
-                </div>
-            </div>
-        </div>';
-            break;
+                    case 1:
+                        $output .= '
+                <div class="container">
+                    <div class="row">
+                    <div class="alert alert-danger alert-dismissible" role="alert">
+                        <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode);" class="close" data-dismiss="alert">
+                        <span aria-hidden="true">×</span>
+                        <span class="sr-only">Close</span>
+                        </button>
+                        <strong><i class="fa fa-warning"></i> Danger!</strong> <marquee><p style="font-family: Impact; font-size: 18pt">Fire on Campus! Evacuate Immediately</p></marquee>
+                        </div>
+                    </div>
+                </div>';
+                    break;
 
-            case 2:
-                $output .= '
-        <div class="container">
-            <div class="row">
-            <div class="alert alert-danger alert-dismissible" role="alert">
-                <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode);" class="close" data-dismiss="alert">
-                <span aria-hidden="true">×</span>
-                <span class="sr-only">Close</span>
-                </button>
-                <strong><i class="fa fa-warning"></i> Danger!</strong> <marquee><p style="font-family: Impact; font-size: 18pt">Tornado on Campus! Take cover</p></marquee>
-                </div>
-            </div>
-        </div>';
-            break;
-            case 3:
-            break;
-            case 4:
-            break;
-            default:
+                    case 2:
+                        $output .= '
+                <div class="container">
+                    <div class="row">
+                    <div class="alert alert-danger alert-dismissible" role="alert">
+                        <button type="button" onclick="this.parentNode.parentNode.removeChild(this.parentNode);" class="close" data-dismiss="alert">
+                        <span aria-hidden="true">×</span>
+                        <span class="sr-only">Close</span>
+                        </button>
+                        <strong><i class="fa fa-warning"></i> Danger!</strong> <marquee><p style="font-family: Impact; font-size: 18pt">Tornado on Campus! Take cover</p></marquee>
+                        </div>
+                    </div>
+                </div>';
+                    break;
+                    case 3:
+                    break;
+                    case 4:
+                    break;
+                    default:
+                
+                }
+
+                
+            }
+
         
-        }
 
         $data = array(
             'alert' => $output,
             'act' => $isact
         );
         echo json_encode($data);
-    }
-        
         oci_free_statement($stid);
     }
 
