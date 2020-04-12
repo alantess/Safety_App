a9f67a0e43ce20cb400cca3ed9d398f6476c7383
JRitalin <r33drift@gmail.com>
2020-04-12 14:49:37 -0400

    spelling error

--- includes/navbar.php
+++ includes/navbar.php
@@ -118,12 +118,12 @@
             </div>
 
             <div class="modal" id="modalVarification">
-                <div class="modal-dialog">
+                <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
 
                         <!-- Modal Header -->
                         <div class="modal-header">
-                            <h4 class="modal-tital">Verify Submition</h4>
+                            <h4 class="modal-title">Verify Submition</h4>
                             <button type="button" class="close" data-dismiss="modal">&times;</button>
                         </div>
 
